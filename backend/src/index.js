b8799c50801ef33961a388f847791315b44179be
deguchi <deguchi.k.110@gmail.com>
2020-07-12 16:32:39 +0900

    add question_page, add auth0

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // define the Express app
 const app = express();
@@ -28,27 +30,48 @@ app.get('/', (req, res) => {
     const qs = questions.map(q => ({
         id: q.id,
         title: q.title,
-        discription: q.desxription,
+        description: q.description,
         answers: q.answers.length,
     }));
     res.send(qs);
 });
 
+app.get('/:id', (req, res) => {
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+    res.send(question[0]);
+});
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dexit.us.auth0.com/.well-known/jwls.json`
+    }),
+
+    audience: '08sRDwrEC9dWVL2vF1XogAt6PqWgSKcX',
+    issuer: `https://dexit.us.auth0.com/`,
+    algorithms: ['RS256']
+});
+
 // insert a new question
-app.post('/', (req, res) => {
+app.post('/', checkJwt, (req, res) => {
     const {title, description} = req.body;
     const newQuestion ={
         id: questions.length + 1,
         title,
         description,
         answers: [],
+        author: req.user.name,
     };
     questions.push(newQuestion);
     res.status(200).send();
 });
 
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
+app.post('/answer/:id', checkJwt, (req, res) => {
     const {answer} = req.body;
     const question = questions.filter(q => (q.id === parseInt(req.params.id)));
     if (question.length > 1) return res.status(500).send();
@@ -56,6 +79,7 @@ app.post('/answer/:id', (req, res) => {
 
     question[0].answers.push({
         answer,
+        author: req.user.name,
     });
 
     res.status(200).send();
